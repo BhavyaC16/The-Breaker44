81bf3f216771f172895a05d1e0b1cb06d8587419
Bhavya <bhavya18333@iiitd.ac.in>
2019-07-11 22:04:45 +0530

    Initial commit

---
+++ Scripts/BallScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce(0, 200f, 0);	 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Scripts/PaddleScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				transform.Translate(10f * Time.deltaTime * Input.GetAxis("Horizontal"), 0, 0);
+		}
+
+	void OnCollisionEnter (Collision colOne){
+		foreach (ContactPoint contact in colOne.contacts){
+			if(contact.thisCollider == collider) {
+				float chill = contact.point.x - transform.position.x;
+
+				contact.otherCollider.rigidbody.AddForce(300f * chill,0,0);
+			}
+		}
+	}
+}
